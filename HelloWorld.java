d837304966980278b918fef75d79564d25cdcb7c
Nandini Reddy <Bhumula.nandini1602@gmail.com>
2024-01-30 09:59:05 -0600

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/** 
+ * My first Java program
+ */
+public class HelloWorld{
+    // main method is the starting point
+    // for your program
+    public static void main(String []args){
+       System.out.println("Hello World");
+    }
+ 
+ 
+ }
+ 
